8a430fe8fe24a09e9249725a52399882715562a6
Ricardo Farias <ricardo.farias@ctctech.com.br>
2023-10-10 23:51:03 -0300

    Correção deploy servidor

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # consisanet-gereciador-tarefas
+## Versão 100% funcional. Realizado o deplyo no servidor.
--- gerenciamento_tarefa/src/main/java/com/consisanet/gerenciamento_tarefa/config/WebMvcConfig.java
+++ gerenciamento_tarefa/src/main/java/com/consisanet/gerenciamento_tarefa/config/WebMvcConfig.java
@@ -9,23 +9,23 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
-//@Configuration
-//public class WebMvcConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//
-//        registry.addResourceHandler("/**/*")
-//                .addResourceLocations("classpath:/static/")
-//                .resourceChain(true)
-//                .addResolver(new PathResourceResolver() {
-//                    @Override
-//                    protected Resource getResource(String resourcePath,
-//                                                   Resource location) throws IOException {
-//                        Resource requestedResource = location.createRelative(resourcePath);
-//                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-//                                : new ClassPathResource("/static/index.html");
-//                    }
-//                });
-//    }
-//}
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+}
